eb2a5aaf999682d6e58ea9d55a837f4b3d1960dd
Parchuk <sergeyparchuk1@gmail.com>
2020-11-20 22:10:44 +0200

    added basic structure

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`));
