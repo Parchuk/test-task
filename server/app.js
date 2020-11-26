d5da7328e7420a08e35afffd0a4e09eefbe9aef6
Parchuk <sergeyparchuk1@gmail.com>
2020-11-26 03:21:35 +0200

    fixed bug img

--- server/app.js
+++ server/app.js
@@ -18,14 +18,10 @@ app.use(cors());
 
 app.use('/api/contact-list', require('./routes/api/ContactList'));
 
-
 // Handle production
 
-
 if (process.env.NODE_ENV === 'production') {
-
     // Static folder
-    app.use('/uploads/', express.static(__dirname + '/public/uploads'));
     app.use(express.static(__dirname + '/public'));
 
     // Handle SPA
